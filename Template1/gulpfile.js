51a1704113943054d731a54d679b289b34a49839
MarcusJetson <MarcusJetson@github.com>
2017-12-16 15:13:16 -0500

    repo templates
    new repo for templates

---
+++ Template1/gulpfile.js
@@ -0,0 +1,9 @@
+
+//************************************
+// Task Syntax here: use this command to create a gulp task
+var gulp = require('gulp');
+gulp.task(nameoftask,functionnoftask(){
+  // does a gulp task
+  console.log('Wow bacon is so juicy');
+});
+//************************************
